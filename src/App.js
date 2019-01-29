857fbd289b50440f11d581238e41479d79c1ef9f
tribeiro <tribeiro@sedronar.gov.ar>
2019-01-29 14:52:56 -0300

    lists & state

--- src/App.js
+++ src/App.js
@@ -13,17 +13,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    console.log("click");
-    // DON'T DO THIS this.state.persons[0].name = "Peregrin";
-    this.setState({
-      persons: [
-        { name: newName , age: 28 },
-        { name: 'Pepe', age: 12 },
-        { name: 'Fede', age: 32}
-      ]})
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -31,6 +20,12 @@ class App extends Component {
         { name: event.target.value, age: 12 },
         { name: 'Fede', age: 32}
       ]})
+  }
+
+  deletePersonHandler = (personIndex) => {
+      const persons = this.state.persons;
+      persons.splice(personIndex, 1);
+      this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -52,9 +47,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            {this.state.persons.map(person => {
-              return <Person name={person.name} 
-              age={person.age}/>
+            {this.state.persons.map((person, index) => {
+              return <Person 
+                click={() => this.deletePersonHandler(index)}
+                name={person.name} 
+                age={person.age}/>
             })}
           </div> 
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import './Person.css';
 const person = (props) => {
     return (
         <div className="Person">
-            <p>I'm {props.name} and i'm {props.age} years old</p>
+            <p onClick={props.click}>I'm {props.name} and i'm {props.age} years old</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}/>   
         </div>
