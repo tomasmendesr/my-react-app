1ef52ac4a28335ed79329469755cd899d3cc59fa
tribeiro <tribeiro@sedronar.gov.ar>
2019-03-26 14:24:47 -0300

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 import withClass from '../hoc/withClass';
-import Aux from '../hoc/Aux';
+import Aux from '../hoc/Auxiliary';
 
 class App extends Component {
   constructor(props){
