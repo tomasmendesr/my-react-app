5457ece1aba1668c18f888516afda9d51d1abb49
tribeiro <tribeiro@sedronar.gov.ar>
2019-04-16 11:48:22 -0300

    using refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,20 +2,42 @@ import React, {Component, Fragment} from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log("[Person.js] rendering...");
         return (
             <Aux> // Same as Fragment
-                <p key="i1" onClick={this.props.click}>I'm {this.props.name} and i'm {this.props.age} years old</p>
+                <p key="i1" onClick={this.props.click}>
+                    I'm {this.props.name} and i'm {this.props.age} years old
+                </p>
                 <p key="i2">{this.props.children}</p>
-                <input key="i3" type='text' 
+                <input key="i3" type='text'
+                    //ref={(inputEl) => {this.inputElement= inputEl}} 
+                    ref={this.inputElementRef}
                     onChange={this.props.changed} 
                     value={this.props.name}/>   
             </Aux>
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
